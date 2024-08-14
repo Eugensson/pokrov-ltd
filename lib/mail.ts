50f399167008512837f2308941962d83dcd682fb
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-08-14 10:57:45 +0300

    Update mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 
-const pass = process.env.EMAIL_PASSWORD;
-const email = process.env.EMAIL;
-const recipient = process.env.EMAIL_RECIPIENT;
+const pass = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+const email = process.env.NEXT_PUBLIC_EMAIL;
+const recipient = process.env.NEXT_PUBLIC_EMAIL_RECIPIENT;
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
